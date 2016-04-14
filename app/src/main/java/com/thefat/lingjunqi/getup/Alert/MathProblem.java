1ba860a5e9e42308179459754e13931e80bb723c
Junqi Ling <junqi.ling@gmail.com>
2016-04-14 17:48:57 +0800

    update code

--- app/src/main/java/com/thefat/lingjunqi/getup/Alert/MathProblem.java
+++ app/src/main/java/com/thefat/lingjunqi/getup/Alert/MathProblem.java
@@ -11,11 +11,6 @@ public class MathProblem {
 	enum Operator {
 		ADD, SUBTRACT, MULTIPLY, DIVIDE;
 
-		/*
-		 * (non-Javadoc)
-		 *
-		 * @see java.lang.Enum#toString()
-		 */
 		@Override
 		public String toString() {
 			String string = null;
@@ -42,6 +37,7 @@ public class MathProblem {
 	private float answer = 0f;
 	private int min = 0;
 	private int max = 10;
+
 	public MathProblem() {
 		this(3);
 	}
@@ -50,82 +46,60 @@ public class MathProblem {
 		super();
 		Random random = new Random(System.currentTimeMillis());
 
-		parts = new ArrayList<Float>(numParts);
+		parts = new ArrayList<>(numParts);
 		for (int i = 0; i < numParts; i++)
 			parts.add(i, (float) random.nextInt(max - min + 1) + min);
 
-		operators = new ArrayList<MathProblem.Operator>(numParts - 1);
+		operators = new ArrayList<>(numParts - 1);
 		for (int i = 0; i < numParts - 1; i++)
-			operators.add(i,Operator.values()[random.nextInt(2)+1]);
+			operators.add(i, Operator.values()[random.nextInt(2) + 1]);
 
-		ArrayList<Object> combinedParts = new ArrayList<Object>();
-		for (int i = 0; i < numParts; i++){
+		ArrayList<Object> combinedParts = new ArrayList<>();
+		for (int i = 0; i < numParts; i++) {
 			combinedParts.add(parts.get(i));
-			if(i<numParts-1)
+			if (i < numParts - 1)
 				combinedParts.add(operators.get(i));
 		}
 
-		while(combinedParts.contains(Operator.DIVIDE)){
+		while (combinedParts.contains(Operator.DIVIDE)) {
 			int i = combinedParts.indexOf(Operator.DIVIDE);
-			answer = (Float)combinedParts.get(i-1) / (Float)combinedParts.get(i+1);
+			answer = (Float) combinedParts.get(i - 1) / (Float) combinedParts.get(i + 1);
 			for (int r = 0; r < 2; r++)
-				combinedParts.remove(i-1);
-			combinedParts.set(i-1, answer);
+				combinedParts.remove(i - 1);
+			combinedParts.set(i - 1, answer);
 		}
-		while(combinedParts.contains(Operator.MULTIPLY)){
+		while (combinedParts.contains(Operator.MULTIPLY)) {
 			int i = combinedParts.indexOf(Operator.MULTIPLY);
-			answer = (Float)combinedParts.get(i-1) * (Float)combinedParts.get(i+1);
+			answer = (Float) combinedParts.get(i - 1) * (Float) combinedParts.get(i + 1);
 			for (int r = 0; r < 2; r++)
-				combinedParts.remove(i-1);
-			combinedParts.set(i-1, answer);
+				combinedParts.remove(i - 1);
+			combinedParts.set(i - 1, answer);
 		}
 
-//		while(combinedParts.contains(Operator.ADD) ||combinedParts.contains(Operator.SUBTRACT)){
-//			int i = 0;
-//			while(!(combinedParts.get(i) instanceof Operator)){
-//				i++;
-//			}
-//			if(combinedParts.get(i) == Operator.ADD){
-//				answer = (Float)combinedParts.get(i-1) + (Float)combinedParts.get(i+1);
-//			}else{
-//				answer = (Float)combinedParts.get(i-1) - (Float)combinedParts.get(i+1);
-//			}
-//			for (int r = 0; r < 2; r++)
-//				combinedParts.remove(i-1);
-//			combinedParts.set(i-1, answer);
-//		}
-
-		while(combinedParts.contains(Operator.ADD)){
+		while (combinedParts.contains(Operator.ADD)) {
 			int i = combinedParts.indexOf(Operator.ADD);
-			answer = (Float)combinedParts.get(i-1) + (Float)combinedParts.get(i+1);
+			answer = (Float) combinedParts.get(i - 1) + (Float) combinedParts.get(i + 1);
 			for (int r = 0; r < 2; r++)
-				combinedParts.remove(i-1);
-			combinedParts.set(i-1, answer);
+				combinedParts.remove(i - 1);
+			combinedParts.set(i - 1, answer);
 		}
-		while(combinedParts.contains(Operator.SUBTRACT)){
+		while (combinedParts.contains(Operator.SUBTRACT)) {
 			int i = combinedParts.indexOf(Operator.SUBTRACT);
-			answer = (Float)combinedParts.get(i-1) - (Float)combinedParts.get(i+1);
+			answer = (Float) combinedParts.get(i - 1) - (Float) combinedParts.get(i + 1);
 			for (int r = 0; r < 2; r++)
-				combinedParts.remove(i-1);
-			combinedParts.set(i-1, answer);
+				combinedParts.remove(i - 1);
+			combinedParts.set(i - 1, answer);
 		}
 
-//		2 5 7 8 9 11
-//		 + - * / -
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.lang.Object#toString()
-	 */
 	@Override
 	public String toString() {
 		StringBuilder problemBuilder = new StringBuilder();
 		for (int i = 0; i < parts.size(); i++) {
 			problemBuilder.append(parts.get(i));
 			problemBuilder.append(" ");
-			if (i < operators.size()){
+			if (i < operators.size()) {
 				problemBuilder.append(operators.get(i).toString());
 				problemBuilder.append(" ");
 			}
